a1ca8dfc2cc891c129b127353ab6c91c611226ec
Wanderson Alves Vieira <wanderson.vieira@netwaninformatica.com.br>
2017-10-20 15:52:58 -0200

    lang_pt.php

---
+++ classes/lang_pt.php
@@ -0,0 +1,55 @@
+<?php
+
+$langHome = "Home";
+
+$langCategorias ="Categorias";
+$langMeuPerfil ="Meu Perfil";
+$langLogin ="Login";
+$langCadastrese ="Cadastre-se";
+$langContato = "Contato";
+$langAvaliacoes ="Avalia&ccedil;&otilde;es";
+$langSair = "Sair";
+$langCadastrodeCliente = "Cadastro de Cliente";
+$langNome = "Nome";
+$langSobrenome = "Sobrenome";
+$langCPF = "CPF";
+$langCNPJ = "CNPJ";
+$langEmail = "E-mail";
+$langTelefone = "Telefone";
+$langCelular = "Celular";
+$langSenha = "Senha";
+$langConfirmarSenha = "Confirmar Senha";
+$langVoltar = "Voltar";
+$langCadastroTransportadora = "Cadastro de Transportadora";
+$langCEP = "CEP";
+$langRua = "RUA";
+$langNumero = "N&uacute;mero";
+$langBairro = "Bairro";
+$langCidade = "Cidade";
+$langEstado = "Estado";
+$langCliqueAqui = "Clique aqui";
+$langNaoSabeCEP = "N&atilde;o Sabe o CEP";
+$lang = "";
+$lang = "";
+$lang = "";
+$lang = "";
+$lang = "";
+$lang = "";
+$lang = "";
+$lang = "";
+
+$langError = "";
+$langError = "";
+$langError = "";
+$langError = "";
+$langError = "";
+$langError = "";
+$langErrorNum = "Digite o N&uacute;mero";
+$langErrorCEP = "DIgitar CEP";
+$langErrorCNPJ = "CNPJ Informado &eacute; inv&aacute;lido";
+
+
+
+
+
+?>
